30895cf0b6bdf43ade9878831d0a3a4190961aba
lsj <lsjjsl004@naver.com>
2025-09-15 13:46:18 +0900

    update

--- src/lab07/like_streamlit.py
+++ src/lab07/like_streamlit.py
@@ -1,6 +1,33 @@
 import streamlit as st
+from openai import OpenAI
+from src.utils import get_openai_api_key
+
+
+def initialize_client():
+    api_key = get_openai_api_key()
+    if not api_key:
+        st.info('API 키가 없습니다.')
+        st.stop()
+
+    client = OpenAI(api_key=api_key)
+
+    return client
+
+
+def get_gpt_response(client, messages):
+    response = client.chat.completions.create(
+        model = 'gpt-4o-mini',
+        temperature = 0.9,
+        messages = messages
+    )
+
+    return response.choices[0].message.content
+
+
 
 def main():
+    client = initialize_client()
+
     st.title('나는 지피티다.')
     st.write('질문해라 닝겐')
 
@@ -16,9 +43,8 @@ def main():
         st.chat_message('user').write(user_input)
         messages.append({'role':'user', 'content': user_input})
 
-        st.chat_message('assistant').write(f'')
-
-
+        st.chat_message('assistant').write(f'답변 - {user_input}')
+        messages.append({'role':'assistant', 'content': f'답변 - {user_input}'})
 
 
 
